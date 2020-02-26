bf814170f55c812d1d13addc4b1cfe008a0416ef
jiteshpatil1992 <jiteshpatil1992@gmail.com>
2020-02-26 13:35:21 +0530

    Add files via upload

---
+++ docs/precache-manifest.f3ef5e9d580ad653ec870b2074513778.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2bd7b669e7806d57280d15673d1b2ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4653609670c3100582e",
+    "url": "/static/css/main.a9e61a59.chunk.css"
+  },
+  {
+    "revision": "a750e1374db473b4887a",
+    "url": "/static/js/2.b5222762.chunk.js"
+  },
+  {
+    "revision": "edd52072abc4c538b7bf61695aa7ad69",
+    "url": "/static/js/2.b5222762.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4653609670c3100582e",
+    "url": "/static/js/main.14c67cc8.chunk.js"
+  },
+  {
+    "revision": "c8509c712f1c12b2cbec",
+    "url": "/static/js/runtime-main.e696ac80.js"
+  },
+  {
+    "revision": "78313ace4e54623190f1ee1b59a24058",
+    "url": "/static/media/logo.78313ace.svg"
+  }
+]);
